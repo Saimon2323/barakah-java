ba87279416989a68ade8446d32c798d2c83d8ae0
Saimon <saimon.ctg@gmail.com>
2023-02-04 23:01:31 +0600

    Add Data-structures Project

---
+++ data-structures/data-structures/src/main/java/com/saimon/TheArrays.java
@@ -0,0 +1,13 @@
+package com.saimon;
+
+/**
+ * @Author Muhammad Saimon
+ * @since 17/04/2022 06:09 PM
+ */
+
+public class TheArrays {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
