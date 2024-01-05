8bd7dedd39f20273de8e7d63621ffb0e69a596f9
Saimon <saimon.ctg@gmail.com>
2024-01-06 01:20:08 +0600

    Add System LineSeparator Example

---
+++ LineSeparator.java
@@ -0,0 +1,11 @@
+/**
+ * Author: Muhammad Saimon
+ * Since 1/6/24 1:17 AM
+ */
+
+public class LineSeparator {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!" + System.lineSeparator() + "Hello World!" + System.lineSeparator() + "Hello World!");
+    }
+}
