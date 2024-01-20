94e76d08b7fba7d7a959615276dd2bb02d545fd6
Saimon <saimon.ctg@gmail.com>
2024-01-21 02:12:23 +0600

    Add Chapter Six code - test Private Constructor

---
+++ javaProgramming/src/chapter/six/EncapOne.java
@@ -0,0 +1,13 @@
+package chapter.six;
+
+/**
+ * Author: Muhammad Saimon
+ * Since 1/19/24 9:21 PM
+ */
+
+public class EncapOne {
+
+    private EncapOne() {
+        System.out.println("EncapOne Constructor");
+    }
+}
---
+++ javaProgramming/src/chapter/six/EncapTwo.java
@@ -0,0 +1,12 @@
+package chapter.six;
+
+/**
+ * Author: Muhammad Saimon
+ * Since 1/19/24 9:22 PM
+ */
+
+public class EncapTwo {
+    public static void main(String[] args) {
+        EncapOne encapOne = new EncapOne();
+    }
+}
