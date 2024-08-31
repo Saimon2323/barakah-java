dc3a2dc73f6e6a269cd46d4b9811c1e1dccbc281
Saimon <saimon.ctg@gmail.com>
2024-09-01 00:32:13 +0600

    Thread Life Cycle - TIMED-WAITING state code updated

--- thread/src/lectureThree/L3ThreadLifeCycleTimedWaitingState.java
+++ thread/src/lectureThree/L3ThreadLifeCycleTimedWaitingState.java
@@ -8,29 +8,28 @@ package lectureThree;
 public class L3ThreadLifeCycleTimedWaitingState implements Runnable {
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("i = " + i + ", State: " + Thread.currentThread().getState());
-            try {
-                Thread.sleep(5_000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            System.out.println("Before Sleep... and State: " + Thread.currentThread().getState());
+            Thread.sleep(8_000);
+            System.out.println("After Sleep timeout - rest of the work... and State: " + Thread.currentThread().getState());
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Thread interrupted..." + e);
         }
     }
 
     public static void main(String[] args) throws Exception {
         L3ThreadLifeCycleTimedWaitingState target = new L3ThreadLifeCycleTimedWaitingState();
         Thread t = new Thread(target, "Thread-lifecycle-2");
-        System.out.println(t.getState() + " " + t.getId() + " " + t.getName()); // NEW
         t.start();
 
         // TIMED_WAITING state is shown here. Because we put the thread to sleep for 5 seconds in run().
         // The thread is in the TIMED_WAITING state when the thread is in the sleep state.
-        Thread.sleep(10_000);
+        Thread.sleep(3_000);
 
         // When Thread waits for some time in the middle of the execution (here 5 seconds in run()), it will be in TIMED_WAITING state.
         // After 5 seconds, the thread will be in the RUNNABLE state.
-        System.out.println(t.getState()); // TIMED_WAITING
+        System.out.println("\nState of " + t.getName() + " :: " + t.getState()); // TIMED_WAITING
+        System.out.println("\nMain end...");
 
     }
 }
