1ae5f38951826e0c29c554f33da6cd7be70770e3
Saimon <saimon.ctg@gmail.com>
2024-11-19 09:38:52 +0600

    Add CPU details and relevant pic

--- thread/src/lectureOne/CPU.java
+++ thread/src/lectureOne/CPU.java
@@ -23,9 +23,9 @@ public class CPU {
 //  - (Expressions are a set of variables and operators)
 
 // CPU works in the following way:
-// 1. Fetch the instruction from the memory
-// 2. Decode the instruction
-// 3. Execute the instruction
+// 1. Fetch the instruction from the primary memory (RAM) or cache memory (L1, L2, L3)
+// 2. Decode the instruction (convert the instruction into a set of operations)
+// 3. Execute the instruction (perform the operations)
 // 4. Store the result back in the memory
 
 // In this example, we have 4 statements.
@@ -34,3 +34,21 @@ public class CPU {
 // to see the bytecode, run the following command in the terminal:
 // $ javap -verbose CPU.class or javap -c CPU.class
 // CPU decodes the bytecode and executes the instructions.
+
+// to see CPU details, run the following command in the terminal:
+// $ lscpu OR cat /proc/cpuinfo
+
+//CPU(s):                   8 // it means 8 cores are available in the CPU (4 physical cores and 4 logical cores) (4*2=8)
+//On-line CPU(s) list:    0-7 // it means 8 cores are available in the CPU (0, 1, 2, 3, 4, 5, 6, 7)
+//Vendor ID:                GenuineIntel // it means the CPU is Intel
+//Model name:             Intel(R) Core(TM) i5-8250U CPU @ 1.60GHz
+//CPU family:           6
+//Model:                142
+//Thread(s) per core:   2 // it means 2 threads are available in each core (4 * 2 = 8 CPUs)
+//Core(s) per socket:   4 // it means 4 cores are available in each socket
+//Socket(s):            1 // it means 1 socket is available in the CPU
+//Caches (sum of all):
+//L1d:                    128 KiB (4 instances) // L1d means Level 1 data cache (4 instances means 4 cores)
+//L1i:                    128 KiB (4 instances) // L1i means Level 1 instruction cache (4 instances means 4 cores)
+//L2:                     1 MiB (4 instances) // L2 means Level 2 cache (4 instances means 4 cores) (1 MiB = 1024 KiB) (4 * 1024 = 4096 KiB)
+//L3:                     6 MiB (1 instance) // L3 means Level 3 cache (1 instance means 1 socket) (6 MiB = 6144 KiB)
